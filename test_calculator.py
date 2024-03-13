59cfd6f44fe592e82438b1aa06dfa35268d1e92e
Max <pengzedong@gmail.com>
2024-03-13 12:23:53 -0600

    update two file

--- test_calculator.py
+++ test_calculator.py
@@ -1,12 +1,15 @@
 # test_calculator.py
 
 import unittest
-from calculator import add
+from calculator import add, subtract
 
 class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(add(5, 3), 8)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
 
 if __name__ == '__main__':
     unittest.main()
